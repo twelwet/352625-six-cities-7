b848429b7e8c5a3b58b9288d0eb003b3266efaae
twelwet <akimovmax@mail.ru>
2021-06-01 20:49:16 +0300

    Добавил первый prop и настроил propTypes

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Main from '../main/main.jsx';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <Main/>;
+function App({placesCount}) {
+  return <Main placesCount={placesCount}/>;
 }
+
+App.propTypes = {
+  placesCount: PropTypes.number,
+};
+
 
 export default App;
--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Item from '../item/item.jsx';
 
-function Main() {
+function Main({placesCount}) {
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -74,7 +75,7 @@ function Main() {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
+              <b className="places__found">{`${placesCount} places to stay in Amsterdam`}</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex="0">
@@ -103,5 +104,9 @@ function Main() {
     </div>
   );
 }
+
+Main.propTypes = {
+  placesCount: PropTypes.number,
+};
 
 export default Main;
