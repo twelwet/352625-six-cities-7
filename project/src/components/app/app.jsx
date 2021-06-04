cd019bff10a6455d6575988e1a46b387aeb74cdf
twelwet <akimovmax@mail.ru>
2021-06-04 16:47:44 +0300

    Добавил маршрутизацию

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import Main from '../pages/main/main.jsx';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Main from '../pages/main/main.jsx';
+import SignIn from '../pages/sign-in/sign-in.jsx';
+import Favourites from '../pages/favourites/favourites.jsx';
+import Room from '../pages/room/room.jsx';
+import NotFound from '../pages/not-found/not-found.jsx';
 
 function App({placesCount}) {
-  return <Main placesCount={placesCount}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={'/'} exact>
+          <Main placesCount={placesCount}/>
+        </Route>
+
+        <Route path={'/login'} exact>
+          <SignIn/>
+        </Route>
+
+        <Route path={'/favourites'} exact>
+          <Favourites/>
+        </Route>
+
+        <Route path={'/offer/:id?'} exact>
+          <Room/>
+        </Route>
+
+        <Route>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
