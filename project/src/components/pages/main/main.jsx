5cdf410b66dcb7e1052d34301a566f712049eed7
twelwet <akimovmax@mail.ru>
2021-06-08 16:23:25 +0300

    Создал новую директорию для переиспользуемых интерфейсных компонентов

--- project/src/components/pages/main/main.jsx
+++ project/src/components/pages/main/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import OfferCard from '../../offer-card/offer-card.jsx';
+import OfferCard from '../../ui/offer-card/offer-card.jsx';
 
 function Main({placesCount}) {
   return (
