2e40a1fcad8f68be76075a07948b3d1f2fbc346f
twelwet <akimovmax@mail.ru>
2021-06-10 17:28:29 +0300

    Создал компонент RatingStar

--- project/src/components/ui/comment-form/comment-form.jsx
+++ project/src/components/ui/comment-form/comment-form.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import RatingStar from '../rating-star/rating-star.jsx';
 
 function CommentForm() {
   const [comment, setComment] = useState({rating: null, review: null});
@@ -21,75 +22,17 @@ function CommentForm() {
     >
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          value="5"
-          id="5-stars"
-          type="radio"
-          onChange={handleFieldChange}
-        />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
-
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          value="4"
-          id="4-stars"
-          type="radio"
-          onChange={handleFieldChange}
-        />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
-
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          value="3"
-          id="3-stars"
-          type="radio"
-          onChange={handleFieldChange}
-        />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
-
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          value="2"
-          id="2-stars"
-          type="radio"
-          onChange={handleFieldChange}
-        />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
-
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          value="1"
-          id="1-star"
-          type="radio"
-          onChange={handleFieldChange}
-        />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"/>
-          </svg>
-        </label>
+        {
+          [5, 4, 3, 2, 1].map(
+            (star) => (
+              <RatingStar
+                key={`${star}-stars`}
+                weight={star}
+                changeHandler={handleFieldChange}
+              />
+            ),
+          )
+        }
       </div>
       <textarea
         className="reviews__textarea form__textarea"
---
+++ project/src/components/ui/rating-star/rating-star.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function RatingStar({weight, changeHandler}) {
+  return (
+    <>
+      <input
+        className="form__rating-input visually-hidden"
+        name="rating"
+        value={weight}
+        id={`${weight}-stars`}
+        type="radio"
+        onChange={changeHandler}
+        defaultChecked={false}
+      />
+      <label htmlFor={`${weight}-stars`} className="reviews__rating-label form__rating-label" title="perfect">
+        <svg className="form__star-image" width="37" height="33">
+          <use xlinkHref="#icon-star"/>
+        </svg>
+      </label>
+    </>
+  );
+}
+
+RatingStar.propTypes = {
+  weight: PropTypes.number,
+  changeHandler: PropTypes.func,
+};
+
+export default RatingStar;
