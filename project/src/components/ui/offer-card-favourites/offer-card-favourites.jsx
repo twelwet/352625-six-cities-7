aac1feaaea38847cda76d4666bf77c0b43c41967
twelwet <akimovmax@mail.ru>
2021-06-15 18:10:36 +0300

    Компонент Карточка 'favourites' проксирует данные из общего компонента

--- project/src/components/ui/offer-card-favourites/offer-card-favourites.jsx
+++ project/src/components/ui/offer-card-favourites/offer-card-favourites.jsx
@@ -1,65 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import offerPropTypes from '../../../prop-types/offer.prop.js';
-import {Link} from 'react-router-dom';
+import OfferCard from '../offer-card/offer-card';
 
-function OfferCardFavourites({data}) {
-  const {
-    id,
-    description,
-    type,
-    price,
-    image,
-    rating,
-    isPremium,
-    isBookmark,
-  } = data;
+function OfferCardFavourites(props) {
+  const {viewData, ...restProps} = props;
 
   return (
-    <article
-      className={'favorites__card place-card'}
-    >
-      <div className={isPremium ? 'place-card__mark' : 'visually-hidden'}>
-        <span>Premium</span>
-      </div>
-      <div
-        className={'favorites__image-wrapper place-card__image-wrapper'}
-      >
-        <Link to={`/offer/${id}`}>
-          <img className="place-card__image" src={image} width={'150'} height={'110'} alt="Some place"/>
-        </Link>
-      </div>
-      <div
-        className={'favorites__card-info place-card__info'}
-      >
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{price}</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
-          </div>
-          <button className={isBookmark ? 'place-card__bookmark-button place-card__bookmark-button--active button' : 'place-card__bookmark-button button'} type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"/>
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
-        </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{width: `${rating}%`}}/>
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
-        <h2 className="place-card__name">
-          <Link to={`/offer/${id}`}>{description}</Link>
-        </h2>
-        <p className="place-card__type">{type}</p>
-      </div>
-    </article>
+    <OfferCard
+      viewData={{
+        cardWidth: '150',
+        cardHeight: '110',
+        classNames: {
+          mainBlock: 'favorites__card',
+          imageBlock: 'favorites__image-wrapper',
+          infoBlock: 'favorites__card-info',
+        },
+      }}
+      {...restProps}
+    />
   );
 }
 
 OfferCardFavourites.propTypes = {
+  viewData: PropTypes.shape({
+    cardWidth: PropTypes.string,
+    cardHeight: PropTypes.string,
+    classNames: PropTypes.shape({
+      mainBlock: PropTypes.string,
+      imageBlock: PropTypes.string,
+      infoBlock: PropTypes.string,
+    }),
+  }),
   data: offerPropTypes,
+  onOfferHover: PropTypes.func,
+  onOfferLeave: PropTypes.func,
 };
 
 export default OfferCardFavourites;
