4636f27699ebd91764bb125014d63a847d1d0f09
twelwet <akimovmax@mail.ru>
2021-06-16 10:33:17 +0300

    Добавил дефолтные значения в проп-тайпы

--- project/src/prop-types/offer-view-data.prop.js
+++ project/src/prop-types/offer-view-data.prop.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 export default PropTypes.shape({
-  cardWidth: PropTypes.string,
-  cardHeight: PropTypes.string,
+  cardWidth: PropTypes.oneOf(['260', '150']),
+  cardHeight: PropTypes.oneOf(['200', '110']),
   classNames: PropTypes.shape({
-    mainBlock: PropTypes.string,
-    imageBlock: PropTypes.string,
-    infoBlock: PropTypes.string,
+    mainBlock: PropTypes.oneOf(['cities__place-card', 'favorites__card', 'near-places__card']),
+    imageBlock: PropTypes.oneOf(['cities__image-wrapper', 'favorites__image-wrapper', 'near-places__image-wrapper']),
+    infoBlock: PropTypes.oneOf(['', 'favorites__card-info']),
   }),
 });
--- project/src/prop-types/offer.prop.js
+++ project/src/prop-types/offer.prop.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default PropTypes.shape({
   id: PropTypes.string,
   description: PropTypes.string,
-  type: PropTypes.string,
+  type: PropTypes.oneOf(['Apartment', 'Private room']),
   price: PropTypes.number,
   image: PropTypes.string,
   rating: PropTypes.number,
