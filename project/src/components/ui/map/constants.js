adf8df0f11b5b3628421f47616f0f98e470e665b
twelwet <akimovmax@mail.ru>
2021-06-18 10:36:44 +0300

    Инициализация карты происходит один раз, перерендер по условию

---
+++ project/src/components/ui/map/constants.js
@@ -0,0 +1,12 @@
+const MapSettings = {
+  LAYER_URL: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+  ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+};
+
+const Icon = {
+  URL: 'img/pin.svg',
+  SIZE: [30, 30],
+  ANCHOR: [15, 20],
+};
+
+export  {MapSettings, Icon};
--- project/src/components/ui/map/map.jsx
+++ project/src/components/ui/map/map.jsx
@@ -1,24 +1,34 @@
-import React, {useEffect} from 'react';
-import initMap from './init-map.js';
+import React, {useRef, useEffect} from 'react';
 import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import useMap from './useMap.js';
 import offersPropTypes from '../../../prop-types/offers.prop.js';
 import cityPropTypes from '../../../prop-types/city.prop';
-import 'leaflet/dist/leaflet.css';
+import {Icon} from './constants.js';
 
 function Map({offers, city}) {
-  useEffect(() => {
-    const {map, icon} = initMap(city);
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
 
-    offers.forEach((offer) => {
-      const {latitude, longitude} = offer.location;
-      leaflet
-        .marker([latitude, longitude], {icon})
-        .addTo(map);
-    });
-  }, [offers, city]);
+  const icon = leaflet.icon({
+    iconUrl: Icon.URL,
+    iconSize: Icon.SIZE,
+    iconAnchor: Icon.ANCHOR,
+  });
+
+  useEffect(() => {
+    if (map) {
+      offers.forEach((offer) => {
+        const {latitude, longitude} = offer.location;
+        leaflet
+          .marker([latitude, longitude], {icon})
+          .addTo(map);
+      });
+    }
+  }, [map, offers, icon]);
 
   return (
-    <div id={'map'} style={{height: '100%'}}/>
+    <div id={'map'} ref={mapRef} style={{height: '100%'}}/>
   );
 }
 
