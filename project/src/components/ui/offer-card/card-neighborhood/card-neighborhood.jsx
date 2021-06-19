d7574c0afaf0eba88412413ac812508fbbb860c8
twelwet <akimovmax@mail.ru>
2021-06-19 13:15:39 +0300

    Упростил компоненты-обертки карточек

--- project/src/components/ui/offer-card/card-neighborhood/card-neighborhood.jsx
+++ project/src/components/ui/offer-card/card-neighborhood/card-neighborhood.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import offerPropTypes from '../../../../prop-types/offer.prop.js';
-import offerViewDataTypes from '../../../../prop-types/offer-view-data.prop.js';
 import OfferCard from '../offer-card.jsx';
 
 function CardNeighborhood(props) {
-  const {viewData, ...restProps} = props;
-
   return (
     <OfferCard
       viewData={{
@@ -18,13 +15,12 @@ function CardNeighborhood(props) {
           infoBlock: '',
         },
       }}
-      {...restProps}
+      {...props}
     />
   );
 }
 
 CardNeighborhood.propTypes = {
-  viewData: offerViewDataTypes,
   data: offerPropTypes,
   onOfferHover: PropTypes.func,
   onOfferLeave: PropTypes.func,
