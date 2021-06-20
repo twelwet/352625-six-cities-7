1e8e00abdc780d5d1d06453789104c41b6b60a7b
twelwet <akimovmax@mail.ru>
2021-06-20 22:07:18 +0300

    Подключил redux к проекту

--- project/src/index.js
+++ project/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import offers from './mocks/offers.js';
 import city from './mocks/city.js';
 import reviews from './mocks/reviews.js';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App offers={offers} city={city} reviews={reviews}/>
+    <Provider store={store}>
+      <App offers={offers} city={city} reviews={reviews}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
