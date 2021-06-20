273b390a117d22b274927a7c8a111e0026d18701
twelwet <akimovmax@mail.ru>
2021-06-20 22:12:10 +0300

    Подключил Redux DevTools

--- project/src/index.js
+++ project/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import offers from './mocks/offers.js';
 import city from './mocks/city.js';
 import reviews from './mocks/reviews.js';
 import {reducer} from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
