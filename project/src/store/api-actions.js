06ea01183f888520d6cad48c7b166b4abe5175f0
twelwet <akimovmax@mail.ru>
2021-06-24 01:17:00 +0300

    Адаптировал данные с сервера

--- project/src/store/api-actions.js
+++ project/src/store/api-actions.js
@@ -3,5 +3,32 @@ import {APIRoute} from '../services/api.js';
 
 export const fetchOffersList = () => (dispatch, _getState, api) => (
   api.get(APIRoute.HOTELS)
-    .then(({data}) => dispatch(ActionCreator.loadOffers(data)))
+    .then(({data}) => {
+      const adoptedData = data.map(
+        (offer) => ({
+          id: offer.id,
+          title: offer.title,
+          description: offer.description,
+          type: offer.type,
+          price: offer.price,
+          previewImage: offer['preview_image'],
+          images: offer.images,
+          rating: offer.rating,
+          bedrooms: offer.bedrooms,
+          maxAdults: offer['max_adults'],
+          goods: offer.goods,
+          host: {
+            id: offer.host.id,
+            name: offer.host.name,
+            isPro: offer.host['is_pro'],
+            avatarUrl: offer.host['avatar_url'],
+          },
+          city: offer.city,
+          isPremium: offer['is_premium'],
+          isFavourite: offer['is_favorite'],
+          location: offer.location,
+        }),
+      );
+      dispatch(ActionCreator.loadOffers(adoptedData));
+    })
 );
