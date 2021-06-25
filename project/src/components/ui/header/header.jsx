f4635ba208930bb9e4826623ed80d43a3c093364
twelwet <akimovmax@mail.ru>
2021-06-25 16:12:52 +0300

    Header зависит от статуса авторизации

--- project/src/components/ui/header/header.jsx
+++ project/src/components/ui/header/header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {AuthorizationStatus} from '../../../constants.js';
 
-function Header() {
+function Header({authorizationStatus}) {
+  const isAuth = authorizationStatus === AuthorizationStatus.AUTH;
   return (
     <header className="header">
       <div className="container">
@@ -14,17 +18,21 @@ function Header() {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <Link className="header__nav-link header__nav-link--profile" to={'/favourites'}>
+                <Link className="header__nav-link header__nav-link--profile" to={isAuth ? '/favourites' : '/login'}>
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                  <span className="header__user-name user__name">{isAuth ? 'Oliver.conner@gmail.com' : 'Sign in'}</span>
                 </Link>
               </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href={'/'}>
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
+              {
+                isAuth ? (
+                  <li className="header__nav-item">
+                    <a className="header__nav-link" href={'/'}>
+                      <span className="header__signout">Sign out</span>
+                    </a>
+                  </li>
+                ) : ''
+              }
             </ul>
           </nav>
         </div>
@@ -33,4 +41,13 @@ function Header() {
   );
 }
 
-export default Header;
+Header.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
+
+export {Header};
+export default connect(mapStateToProps, null)(Header);
