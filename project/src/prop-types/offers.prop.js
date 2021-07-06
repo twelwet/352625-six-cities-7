e3f1bd13983c865827635034eb7ef1b3b14b5b0e
twelwet <akimovmax@mail.ru>
2021-07-06 15:37:12 +0300

    Поправил проп-тайпы для offers

--- project/src/prop-types/offers.prop.js
+++ project/src/prop-types/offers.prop.js
@@ -1,4 +1,10 @@
 import PropTypes from 'prop-types';
 import offerPropTypes from './offer.prop.js';
 
-export default PropTypes.arrayOf(offerPropTypes).isRequired;
+export default PropTypes.shape({
+  status: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(offerPropTypes).isRequired,
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }).isRequired,
+});
