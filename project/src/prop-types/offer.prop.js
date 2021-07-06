a1195c43ec20557f2304eecd5beb99101d071a09
twelwet <akimovmax@mail.ru>
2021-07-06 17:34:55 +0300

    Поправил проп-тайпы для offer

--- project/src/prop-types/offer.prop.js
+++ project/src/prop-types/offer.prop.js
@@ -1,38 +1,10 @@
 import PropTypes from 'prop-types';
-import cityPropTypes from './city.prop.js';
-import {StayType} from '../constants.js';
+import offerDataPropTypes from './offer-data.prop';
 
 export default PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  type: PropTypes.oneOf([StayType.APARTMENT, StayType.ROOM, StayType.HOTEL, StayType.HOUSE]).isRequired,
-  price: PropTypes.number.isRequired,
-  previewImage: PropTypes.string.isRequired,
-  images: PropTypes.arrayOf(PropTypes.string).isRequired,
-  rating: PropTypes.number.isRequired,
-  bedrooms: PropTypes.number.isRequired,
-  maxAdults: PropTypes.number.isRequired,
-  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
-  host: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    isPro: PropTypes.boolean,
-    avatarUrl: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  data: offerDataPropTypes,
+  error: PropTypes.shape({
+    message: PropTypes.string,
   }).isRequired,
-  city: PropTypes.shape({
-    name: cityPropTypes,
-    location: PropTypes.shape({
-      latitude: PropTypes.number.isRequired,
-      longitude: PropTypes.number.isRequired,
-      zoom: PropTypes.number.isRequired,
-    }),
-  }).isRequired,
-  location: PropTypes.shape({
-    latitude: PropTypes.number.isRequired,
-    longitude: PropTypes.number.isRequired,
-    zoom: PropTypes.number.isRequired,
-  }).isRequired,
-  isPremium: PropTypes.boolean,
-  isFavourite: PropTypes.boolean,
-}).isRequired;
+});
--- project/src/prop-types/offers.prop.js
+++ project/src/prop-types/offers.prop.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import offerPropTypes from './offer.prop.js';
+import offerDataPropTypes from './offer-data.prop.js';
 
 export default PropTypes.shape({
   status: PropTypes.string.isRequired,
-  data: PropTypes.arrayOf(offerPropTypes).isRequired,
+  data: PropTypes.arrayOf(offerDataPropTypes).isRequired,
   error: PropTypes.shape({
     message: PropTypes.string,
   }).isRequired,
