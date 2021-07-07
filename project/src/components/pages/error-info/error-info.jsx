c94c5f2d914f5e287f9557f242aac608258a67bb
twelwet <akimovmax@mail.ru>
2021-07-07 15:57:42 +0300

    Экран ошибки может рендерить несколько сообщений об ошибках

--- project/src/components/pages/error-info/error-info.jsx
+++ project/src/components/pages/error-info/error-info.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {AppRoute} from '../../../constants.js';
 
-function ErrorInfo({error}) {
+function ErrorInfo({errors}) {
   return (
     <div className="page page--gray page--main">
-      <h1 style={{textAlign: 'center'}}>
-        <div>{error.message}</div>
-      </h1>
+      {
+        errors.map(
+          (error) =>
+            (
+              <h1 key={error.message} style={{textAlign: 'center'}}>
+                <div>{error.message}</div>
+              </h1>
+            ),
+        )
+      }
       <a href={AppRoute.MAIN} style={{textAlign: 'center'}}>На главную</a>
     </div>
   );
 }
 
 ErrorInfo.propTypes = {
-  error: PropTypes.shape({
-    message: PropTypes.string.isRequired,
-  }).isRequired,
+  errors: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string,
+    }).isRequired,
+  ).isRequired,
 };
 
 export default ErrorInfo;
