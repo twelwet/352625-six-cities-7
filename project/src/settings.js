e74c31ee5cc237509bad1e832225ced06f8a3af1
twelwet <akimovmax@mail.ru>
2021-07-10 12:54:10 +0300

    Ограничил кол-во комментариев

--- project/src/settings.js
+++ project/src/settings.js
@@ -2,5 +2,6 @@ import {CityName} from './constants.js';
 
 const RATING_STARS = 5;
 const DEFAULT_CITY = CityName.PARIS;
+const COMMENTS_TO_SHOW = 10;
 
-export {RATING_STARS, DEFAULT_CITY};
+export {RATING_STARS, DEFAULT_CITY, COMMENTS_TO_SHOW};
