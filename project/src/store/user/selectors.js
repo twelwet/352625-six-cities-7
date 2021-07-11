30b08026653f783180005a74afbeb7d688249f7d
twelwet <akimovmax@mail.ru>
2021-07-11 21:59:09 +0300

    Поправил селекторы - продолжение

--- project/src/store/user/selectors.js
+++ project/src/store/user/selectors.js
@@ -2,5 +2,5 @@ import {NameSpace} from '../root-reducer';
 
 export const getAuthStatus = (state) => state[NameSpace.USER].authorizationStatus;
 export const getUserCommentStatus = (state) => state[NameSpace.USER].userComment;
-export const getLoginStatus = (state) => state[NameSpace.USER].login.status;
+export const getLogin = (state) => state[NameSpace.USER].login;
 export const getAuthInfo = (state) => state[NameSpace.USER].authInfo;
