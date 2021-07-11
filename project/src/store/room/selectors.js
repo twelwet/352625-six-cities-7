15c8fcd89ad2ac1cf766a794f74c48b10de1fe96
twelwet <akimovmax@mail.ru>
2021-07-11 21:55:13 +0300

    Поправил селекторы

--- project/src/store/room/selectors.js
+++ project/src/store/room/selectors.js
@@ -1,13 +1,16 @@
 import {NameSpace} from '../root-reducer';
 
+export const getOffer = (state) => state[NameSpace.ROOM].offer;
 export const getOfferData = (state) => state[NameSpace.ROOM].offer.data;
 export const getOfferStatus = (state) => state[NameSpace.ROOM].offer.status;
 export const getOfferError = (state) => state[NameSpace.ROOM].offer.error;
 
-export const getNeighborOffersData = (state) => state[NameSpace.OFFERS].neighborOffers.data;
-export const getNeighborOffersStatus = (state) => state[NameSpace.OFFERS].neighborOffers.status;
-export const getNeighborOffersError = (state) => state[NameSpace.OFFERS].neighborOffers.error;
+export const getNeighborOffers = (state) => state[NameSpace.ROOM].neighborOffers;
+export const getNeighborOffersData = (state) => state[NameSpace.ROOM].neighborOffers.data;
+export const getNeighborOffersStatus = (state) => state[NameSpace.ROOM].neighborOffers.status;
+export const getNeighborOffersError = (state) => state[NameSpace.ROOM].neighborOffers.error;
 
-export const getReviewsData = (state) => state[NameSpace.OFFERS].reviews.data;
-export const getReviewsStatus = (state) => state[NameSpace.OFFERS].reviews.status;
-export const getReviewsError = (state) => state[NameSpace.OFFERS].reviews.error;
+export const getReviews = (state) => state[NameSpace.ROOM].reviews;
+export const getReviewsData = (state) => state[NameSpace.ROOM].reviews.data;
+export const getReviewsStatus = (state) => state[NameSpace.ROOM].reviews.status;
+export const getReviewsError = (state) => state[NameSpace.ROOM].reviews.error;
