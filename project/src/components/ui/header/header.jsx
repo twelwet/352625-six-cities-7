7598a6e3c1f17799cc9559d5fac68c239cb8c4b9
twelwet <akimovmax@mail.ru>
2021-07-13 13:02:47 +0300

    Заменил маршруты на константы маршрутов

--- project/src/components/ui/header/header.jsx
+++ project/src/components/ui/header/header.jsx
@@ -26,7 +26,7 @@ function Header({authorizationStatus, authInfo, onSignOut}) {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <Link className="header__nav-link header__nav-link--profile" to={isAuth ? '/favourites' : '/login'}>
+                <Link className="header__nav-link header__nav-link--profile" to={isAuth ? AppRoute.FAVOURITES : AppRoute.LOGIN}>
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
                   <span className="header__user-name user__name">{isAuth ? authInfo.email : 'Sign in'}</span>
