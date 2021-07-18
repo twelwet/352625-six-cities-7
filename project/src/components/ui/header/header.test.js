0a881e6f64c1402f04d006eff06c8db5973baf0b
twelwet <akimovmax@mail.ru>
2021-07-18 23:19:38 +0300

    Добавил кейс теста NO_AUTH для Header

--- project/src/components/ui/header/header.test.js
+++ project/src/components/ui/header/header.test.js
@@ -5,7 +5,7 @@ import {createMemoryHistory} from 'history';
 import {Header} from './header.jsx';
 
 describe('Component: Header', () => {
-  it('should render correctly', () => {
+  it('should render correctly (AUTH mode)', () => {
     const history = createMemoryHistory();
     const authorizationStatus = 'AUTH';
     const authInfo = {
@@ -25,4 +25,26 @@ describe('Component: Header', () => {
 
     expect(getByText('Sign out')).toBeInTheDocument();
   });
+
+  it('should render correctly (NO_AUTH mode)', () => {
+    const history = createMemoryHistory();
+    const authorizationStatus = 'NO_AUTH';
+    const authInfo = {
+      id: 1,
+      email: 'email',
+      name: 'name',
+      avatarUrl: 'avatarUrl',
+      isPro: true,
+      token: '',
+    };
+
+    const {getByText} = render(
+      <Router history={history}>
+        <Header authorizationStatus={authorizationStatus} authInfo={authInfo} onSignOut={() => {}}/>
+      </Router>,
+    );
+
+    expect(getByText('Sign in')).toBeInTheDocument();
+  });
+
 });
