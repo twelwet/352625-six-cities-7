7022cf943a823c4bf6c769e22d3fa220ee8aacf3
twelwet <akimovmax@mail.ru>
2021-07-22 22:55:59 +0300

    Добавил адаптер данных в тест экшена pushFavouriteStatus

--- project/src/store/test-api-actions/push-favourite-status.test.js
+++ project/src/store/test-api-actions/push-favourite-status.test.js
@@ -3,6 +3,8 @@ import createAPI from '../../services/api.js';
 import {ActionType} from '../action.js';
 import {pushFavouriteStatus} from '../api-actions.js';
 import {APIRoute, HttpCode} from '../../constants.js';
+import getOfferAdapter from '../../utils/get-offer-adapter.js';
+import {StayType} from '../../constants.js';
 
 let api = null;
 
@@ -12,8 +14,41 @@ describe('Async operation pushFavouriteStatus(offerId, status)', () => {
   });
 
   it(`should make a correct API call to POST '/favorite/1/1' (statusCode: ${HttpCode.OK})`, () => {
-    // TODO не пойму почему тест не проходит
-    const offerBefore = {id: 1, title: 'offer1', isFavourite: false};
+    const offerBefore = {
+      id: 1,
+      title: 'offer1',
+      description: 'Some description of offer1',
+      type: StayType.HOUSE,
+      price: 100,
+      previewImage: '/img/offer1-preview1.png',
+      images: ['/img/offer1-preview1.png', '/img/offer1-preview2.png', '/img/offer1-preview3.png'],
+      rating: 4,
+      bedrooms: 2,
+      maxAdults: 2,
+      goods: ['W-Fi', 'TV', 'Towels'],
+      host: {
+        id: 1,
+        name: 'John',
+        isPro: false,
+        avatarUrl: 'img/avatar1.png',
+      },
+      city: {
+        name: 'Paris',
+        location: {
+          latitude: 52,
+          longitude: 24,
+          zoom: 3,
+        },
+      },
+      location: {
+        latitude: 52,
+        longitude: 24,
+        zoom: 3,
+      },
+      isPremium: 'true',
+      isFavourite: false,
+    };
+
     const offer = {...offerBefore, isFavourite: true};
 
     const apiMock = new MockAdapter(api);
@@ -30,21 +65,12 @@ describe('Async operation pushFavouriteStatus(offerId, status)', () => {
 
     return favStatusPusher(dispatch, getState, api)
       .then(() => {
-        // TODO ожидаемое поведение:
-        // expect(dispatch).toHaveBeenCalledTimes(1);
-        //
-        // expect(dispatch).toHaveBeenNthCalledWith(1, {
-        //   type: ActionType.UPDATE_OFFER_FULFILLED,
-        //   payload: offer,
-        // });
-
-        // TODO поведение по факту:
         expect(dispatch).toHaveBeenCalledTimes(1);
 
         expect(dispatch).toHaveBeenNthCalledWith(1, {
-          type: ActionType.UPDATE_OFFER_REJECTED,
+          type: ActionType.UPDATE_OFFER_FULFILLED,
+          payload: getOfferAdapter(offer),
         });
-
       });
   });
 
