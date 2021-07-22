536f56e0564bdb9d171ee94c17e11ace21d1b90b
twelwet <akimovmax@mail.ru>
2021-07-22 22:50:59 +0300

    Поправил тест экшена pushComment

--- project/src/store/test-api-actions/push-comment.test.js
+++ project/src/store/test-api-actions/push-comment.test.js
@@ -3,6 +3,8 @@ import createAPI from '../../services/api.js';
 import {ActionType} from '../action.js';
 import {pushComment} from '../api-actions.js';
 import {APIRoute, HttpCode} from '../../constants.js';
+import getAdaptedData from '../../utils/get-adapted-data.js';
+import getCommentAdapter from '../../utils/get-comment-adapter.js';
 
 let api = null;
 
@@ -12,47 +14,53 @@ describe('Async operation pushComment(offerId)', () => {
   });
 
   it(`should make a correct API call to POST '/comments/1' (statusCode: ${HttpCode.OK})`, () => {
-    // TODO не пойму почему тест не проходит
-    const commentsBefore = [{id: 1, title: 'comment1'}, {id: 2, title: 'comment2'}];
-    const newComment = {id: 3, title: 'comment3'};
-    const commentsFromServer = commentsBefore.concat(newComment);
+    const commentData = {comment: 'Some comment text 3', rating: 5};
+
+    const commentsFromServer = [{
+      id: 1,
+      date: '2021-06-30T16:51:35.215Z',
+      rating: 4,
+      comment: 'Some comment text 1',
+      user: {id: 1, name: '', isPro: true, avatarUrl: 'img/avatar1.png'},
+    }, {
+      id: 2,
+      date: '2021-05-06T14:00:06.215Z',
+      rating: 5,
+      comment: 'Some comment text 2',
+      user: {id: 4, name: '', isPro: false, avatarUrl: 'img/avatar4.png'},
+    }, {
+      id: 3,
+      date: '2021-05-25T13:49:20.215Z',
+      rating: 4,
+      comment: 'Some comment text 3',
+      user: {id: 2, name: '', isPro: false, avatarUrl: 'img/avatar2.png'},
+    }];
 
     const apiMock = new MockAdapter(api);
     const dispatch = jest.fn();
-    const commentPusher = pushComment(1);
+    const commentPusher = pushComment(commentData, 1);
 
     const getState = () => ({
       USER: {authInfo: {token: '12345'}},
     });
 
     apiMock
-      .onPost(APIRoute.COMMENTS, newComment)
+      .onPost(`${APIRoute.COMMENTS}/1`)
       .reply(HttpCode.OK, commentsFromServer);
 
     return commentPusher(dispatch, getState, api)
       .then(() => {
-        // TODO ожидаемое поведение:
-        // expect(dispatch).toHaveBeenCalledTimes(3);
-        //
-        // expect(dispatch).toHaveBeenNthCalledWith(1, {
-        //   type: ActionType.PUSH_COMMENT_PENDING,
-        // });
-        // expect(dispatch).toHaveBeenNthCalledWith(2, {
-        //   type: ActionType.SAVE_COMMENTS,
-        //   payload: commentsFromServer,
-        // });
-        // expect(dispatch).toHaveBeenNthCalledWith(3, {
-        //   type: ActionType.PUSH_COMMENT_IDLE,
-        // });
-
-        // TODO поведение по факту:
-        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch).toHaveBeenCalledTimes(3);
 
         expect(dispatch).toHaveBeenNthCalledWith(1, {
           type: ActionType.PUSH_COMMENT_PENDING,
         });
         expect(dispatch).toHaveBeenNthCalledWith(2, {
-          type: ActionType.PUSH_COMMENT_REJECTED,
+          type: ActionType.SAVE_COMMENTS,
+          payload: getAdaptedData(commentsFromServer, getCommentAdapter),
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: ActionType.PUSH_COMMENT_IDLE,
         });
       });
   });
