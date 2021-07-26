bdd29ad5c50022071a0ca24d876762f80de52052
twelwet <akimovmax@mail.ru>
2021-07-27 00:41:28 +0300

    Добавил тест для CardFavourites

---
+++ project/src/components/ui/offer-card/card-favourites/card-favourites.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CardFavourites from './card-favourites.jsx';
+import {CityName, StayType} from '../../../../constants';
+
+const getFakeOfferCard = () => (
+  <article data-testid={'fake-offer-card'}/>
+);
+
+jest.mock('../offer-card', () => getFakeOfferCard);
+
+describe('Component: CardFavourites', () => {
+  it('should proxy FakeOfferCard correctly', () => {
+    const offerData = {
+      id: 1,
+      title: 'Offer title example',
+      description: 'description',
+      type: StayType.HOUSE,
+      price: 100,
+      previewImage: 'previewImage',
+      images: ['previewImage1', 'previewImage2'],
+      rating: 4,
+      bedrooms: 3,
+      maxAdults: 2,
+      goods: ['good1', 'good2'],
+      host: {
+        id: 1,
+        name: 'name',
+        isPro: true,
+        avatarUrl: 'avatarUrl',
+      },
+      city: {
+        name: CityName.PARIS,
+        location: {
+          latitude: 1,
+          longitude: 1,
+          zoom: 1,
+        },
+      },
+      location: {
+        latitude: 1,
+        longitude: 1,
+        zoom: 1,
+      },
+      isPremium: true,
+      isFavourite: true,
+    };
+
+    render(<CardFavourites data={offerData}/>);
+
+    expect(screen.getByTestId('fake-offer-card')).toBeInTheDocument();
+  });
+});
