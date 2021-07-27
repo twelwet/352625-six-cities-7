cd1ade59deb320372454241cc194c977a29f45c2
twelwet <akimovmax@mail.ru>
2021-07-27 14:17:41 +0300

    Расширил тест для СommentForm

--- project/src/components/ui/comment-form/comment-form.test.js
+++ project/src/components/ui/comment-form/comment-form.test.js
@@ -3,11 +3,12 @@ import {render, screen} from '@testing-library/react';
 import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
 import userEvent from '@testing-library/user-event';
-import {CommentForm} from './comment-form.jsx';
+import {CommentForm} from './comment-form';
 import {Status, StayType, CityName} from '../../../constants.js';
 
 describe('Component: CommentForm', () => {
-  it('should render correctly', () => {
+  it('should render correctly and can send comment', () => {
+    const handleClick = jest.fn(() => Promise.resolve(42));
     const userComment = {status: Status.IDLE};
     const offer = {
       status: Status.IDLE,
@@ -54,14 +55,77 @@ describe('Component: CommentForm', () => {
 
     render(
       <Router history={history}>
-        <CommentForm saveReview={() => {}} offer={offer} userComment={userComment} />
+        <CommentForm saveReview={handleClick} offer={offer} userComment={userComment} />
       </Router>,
     );
 
     const reviewLabel = screen.getByText('Your review');
-    userEvent.type(screen.getByTestId('comment'), 'Текст комментария');
-
     expect(reviewLabel).toBeInTheDocument();
-    expect(screen.getByDisplayValue(/Текст комментария/i)).toBeInTheDocument();
+
+    expect(screen.queryByDisplayValue(/Текст комментария/i)).not.toBeInTheDocument();
+    userEvent.type(screen.getByTestId('comment'), 'Текст комментария');
+    expect(screen.queryByDisplayValue(/Текст комментария/i)).toBeInTheDocument();
+
+    const ratingRadio = [...screen.getAllByTestId('rating-radio')][1];
+    userEvent.click(ratingRadio);
+
+    expect(handleClick).not.toBeCalled();
+    userEvent.click(screen.getByTestId('send-comment'));
+    expect(handleClick).toBeCalled();
+  });
+
+  it('should render special Notification when comment is rejected by server', () => {
+    const handleClick = jest.fn(() => Promise.resolve(42));
+    const userComment = {status: Status.REJECTED};
+    const offer = {
+      status: Status.IDLE,
+      data: {
+        id: 1,
+        title: 'title',
+        description: 'description',
+        type: StayType.HOUSE,
+        price: 100,
+        previewImage: 'previewImage',
+        images: ['previewImage1', 'previewImage2'],
+        rating: 4,
+        bedrooms: 3,
+        maxAdults: 2,
+        goods: ['good1', 'good2'],
+        host: {
+          id: 1,
+          name: 'name',
+          isPro: true,
+          avatarUrl: 'avatarUrl',
+        },
+        city: {
+          name: CityName.PARIS,
+          location: {
+            latitude: 1,
+            longitude: 1,
+            zoom: 1,
+          },
+        },
+        location: {
+          latitude: 1,
+          longitude: 1,
+          zoom: 1,
+        },
+        isPremium: true,
+        isFavourite: true,
+      },
+      error: {
+        message: null,
+      },
+    };
+
+    const history = createMemoryHistory();
+
+    const {getByText} = render(
+      <Router history={history}>
+        <CommentForm saveReview={handleClick} offer={offer} userComment={userComment} />
+      </Router>,
+    );
+
+    expect(getByText('Mark the rating or write more characters')).toBeInTheDocument();
   });
 });
